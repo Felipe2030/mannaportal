3162ee10bb801a5db5522596487f0ade2b28bbdb
Felipe Henrique de Oliveira Silva Bonfim <44967449+Felipe2030@users.noreply.github.com>
2022-07-07 19:18:44 -0300

    init

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('servicos de api!');
+    res.send('servicos de api! v2');
 });
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}!`))
