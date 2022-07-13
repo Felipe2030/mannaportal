9658f0df9de8d9df5bb1b4fe1ab45ab1363bad32
Felipe Henrique de Oliveira Silva Bonfim <44967449+Felipe2030@users.noreply.github.com>
2022-07-12 22:30:42 -0300

    start

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+const MysqlConnection = require("./utils/database/mysql-connection")
 const routers = require('./routers/index')
 const express = require('express')
 const http = require("http")
@@ -11,5 +12,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(routers)
 
 server.listen(port, () => {
+    new MysqlConnection().migrations()
     console.log(`Servidor rodando na porta ${port}!`)
 })
